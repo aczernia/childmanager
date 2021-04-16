a23b351e02565e50a8223255654e99a596fd6321
KamilIrzenski <kirzenski@gmail.com>
2021-04-16 22:24:31 +0200

    add Jwt Token and Login

---
+++ ChildManager.Backend/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChildManager.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ChildManager.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (BadRequestException badRequestException)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestException.Message);
+                
+            }
+        }
+    }
+}
--- ChildManager.Backend/Startup.cs
+++ ChildManager.Backend/Startup.cs
@@ -1,16 +1,18 @@
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ChildManager.Entities;
+using ChildManager.Middleware;
 using ChildManager.Models;
 using ChildManager.Models.Validators;
 using ChildManager.Services;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Identity;
-
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace ChildManager
@@ -39,7 +41,28 @@ namespace ChildManager
                 });
             });
 
+            var authenticationSettings = new AuthenticationSettings();
 
+            Configuration.GetSection("Authentication").Bind(authenticationSettings);
+            services.AddSingleton(authenticationSettings);
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = "Bearer";
+                option.DefaultScheme = "Bearer";
+                option.DefaultChallengeScheme = "Bearer";
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = authenticationSettings.JwtIssuer,
+                    ValidAudience = authenticationSettings.JwtIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+                };
+            });
+            
             services.AddControllers().AddFluentValidation();
             services.AddDbContext<ChildManagerDbContext>();
             services.AddAutoMapper(this.GetType().Assembly);
@@ -48,6 +71,7 @@ namespace ChildManager
             services.AddTransient<IClassService, ClassService>();
             services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+            services.AddScoped<ErrorHandlingMiddleware>();
             services.AddScoped<ChildSeeder>();
             services.AddScoped<IAccountService, AccountService>();
         }
@@ -61,6 +85,7 @@ namespace ChildManager
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseHttpsRedirection();
 
             app.UseRouting();
